42e2f029da34f2a4bf96c648b5786b423f294a34
analuzluz <43620508+analuzluz@users.noreply.github.com>
2018-10-03 16:29:46 -0300

    modificacao da aula 03/10

--- rest_blog/api/categoria/read.php
+++ rest_blog/api/categoria/read.php
@@ -1,25 +1,42 @@
 <?php
+ 	if(!isset($_SERVER['PHP_AUTH_USER'])){
+ 		header('WWW-Authenitcate: Basic realm="Página Restrita"');
 
-	header('Acess-Control-Allow-Origin: *');
-	header('Content-Type: application/json');
-	
-	require_once '../../config/Conexao.php';
-	require_once '../../models/Categoria.php';
+ 		header('HTTP/1.0 401 Unauthorized');
 
-	$db = new Conexao();
-	$con = $db->getConexao();
+ 		echo json_encode(["mensagem" => "Autenticação necessária"
+ 			]);
 
-    $categoria = new Categoria($con);
+ 		exit;
+ 	}elseif (!($_SERVER['PHP_AUTH_USER'] == 'admin'
+ 			 && $_SERVER['PHP_AUTH_PW']	== 'admin')){
+ 				header('HTTP/1.0 401 Unauthorized');
+				echo json_encode(["mensagem" => "Usuário Inválido"]);
+ 	}else{
+ 			
 
-    $resultado = $categoria->read();
+		header('Acess-Control-Allow-Origin: *');
+		header('Content-Type: application/json');
+		
+		require_once '../../config/Conexao.php';
+		require_once '../../models/Categoria.php';
 
-    $qtde_cats = sizeof($resultado);
+		$db = new Conexao();
+		$con = $db->getConexao();
 
-    if($qtde_cats>0){
-        // $arr_categorias = array();
-        // $arr_categorias['data'] = array();
+	    $categoria = new Categoria($con);
 
-        echo json_encode($resultado);
-    }else{
-        echo json_encode(array('mensagem' => 'nenhuma categoria encontrada'));
-    }
+	    $resultado = $categoria->read();
+
+	    $qtde_cats = sizeof($resultado);
+
+	    if($qtde_cats>0){
+	        // $arr_categorias = array();
+	        // $arr_categorias['data'] = array();
+
+	        echo json_encode($resultado);
+	    }else{
+	        echo json_encode(array('mensagem' => 'nenhuma categoria encontrada'));
+	    }
+
+   	}
